70665d3e0f1f824a211f766136eec9ebbf4eba2b
https://github.com/as5293 <as5293@srmist.edu.in>
2026-04-07 09:34:27 +0530

    UC19: Binary Search for Bogie ID (Optimized Searching)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -3,20 +3,31 @@ public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
         System.out.println("======================================");
-        System.out.println("UC18 - Linear Search for Bogie ID");
+        System.out.println("UC19 - Binary Search for Bogie ID");
         System.out.println("======================================\n");
 
 
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
 
+
         String searchKey = "BG309";
 
+        int low = 0;
+        int high = bogieIds.length - 1;
         boolean found = false;
 
-        for (String id : bogieIds) {
-            if (id.equals(searchKey)) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int result = bogieIds[mid].compareTo(searchKey);
+
+            if (result == 0) {
                 found = true;
                 break;
+            } else if (result < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
@@ -26,6 +37,6 @@ public class TrainConsistManagementApp {
             System.out.println("Bogie ID " + searchKey + " NOT found.");
         }
 
-        System.out.println("\nUC18 search completed...");
+        System.out.println("\nUC19 search completed...");
     }
 }
