24532f2ac2e293c8aab71ef00bb3be2a0fdb1616
https://github.com/as5293 <as5293@srmist.edu.in>
2026-04-07 09:38:15 +0530

    UC20: Exception Handling During Search Operations

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -3,40 +3,34 @@ public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
         System.out.println("======================================");
-        System.out.println("UC19 - Binary Search for Bogie ID");
+        System.out.println("UC20 - Exception Handling in Search");
         System.out.println("======================================\n");
 
 
-        String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
+        String[] bogieIds = {};
+
+        String searchKey = "BG101";
 
 
-        String searchKey = "BG309";
+        if (bogieIds.length == 0) {
+            throw new IllegalStateException("No bogies available. Cannot perform search.");
+        }
 
-        int low = 0;
-        int high = bogieIds.length - 1;
         boolean found = false;
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            int result = bogieIds[mid].compareTo(searchKey);
-
-            if (result == 0) {
+        for (String id : bogieIds) {
+            if (id.equals(searchKey)) {
                 found = true;
                 break;
-            } else if (result < 0) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
             }
         }
 
         if (found) {
-            System.out.println("Bogie ID " + searchKey + " found in the train.");
+            System.out.println("Bogie ID " + searchKey + " found.");
         } else {
             System.out.println("Bogie ID " + searchKey + " NOT found.");
         }
 
-        System.out.println("\nUC19 search completed...");
+        System.out.println("\nUC20 execution completed...");
     }
 }
